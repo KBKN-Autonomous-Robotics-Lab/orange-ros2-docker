7ca23b270f735dd1330a3920b7f8f0edde951c32
Alpaca-zip <zip.lottestr@gmail.com>
2023-06-28 13:17:08 +0900

    delete unused port

--- run.sh
+++ run.sh
@@ -1,9 +1,5 @@
 docker run \
 	-p 6080:80 \
-	-p 2222:22 \
-	-p 10940:10940 \
-	-p 2368:2368/udp \
-	-p 8308:8308/udp \
 	--shm-size=512m \
 	--security-opt seccomp=unconfined \
 	--device /dev/ZLAC8015D:/dev/ZLAC8015D:mwr \
