7b340839a9236b565a07eef57fbb3d6a547cd28e
shunki1006 <shunki.shibuya.5v@stu.hosei.ac.jp>
2023-07-04 08:13:48 +0000

    rename_image

--- runLite.sh
+++ runLite.sh
@@ -6,6 +6,6 @@ docker run \
     -p 8308:8308/udp \
     --shm-size=512m \
     --security-opt seccomp=unconfined \
-    orange_ros2
+    kbkn202x/orange_ros2:latest
     
     #-e RESOLUTION=1920x1080 \
